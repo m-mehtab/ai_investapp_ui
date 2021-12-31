aab2eef53fff0d31027194be0af12f07b5b9cd61
Maaz Mehtab <maaz.mehtab@axialideas.com>
2022-01-01 03:12:12 +0500

    add fund route

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,11 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
-// import Login from './components/Auth/Login';
-// import Signup from './components/Auth/Signup';
-// import ForgotPassword from './components/Auth/ForgotPassword';
-
 import Forgot from './pages/Auth/Forgot';
 import Login from './pages/Auth/Login';
 import Signup from './pages/Auth/Signup';
 import Dashboard from './pages/Dashboard/index';
+import Funds from './pages/Funds/index';
 import history from './History';
 export default function RouteFile() {
     return (
@@ -19,6 +16,9 @@ export default function RouteFile() {
                 </Route>
                 <Route path="/Dashboard" component={Dashboard}>
                     <Dashboard />
+                </Route>
+                <Route path="/Funds" component={Funds}>
+                    <Funds />
                 </Route>
 
                 <Route path="/" component={Dashboard}>
